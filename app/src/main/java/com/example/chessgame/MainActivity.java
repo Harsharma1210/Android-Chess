db861dc3b3b6a83af2b3214eb7d1faed22c2aa75
Harsh Sharma <43477089+Harsharma1210@users.noreply.github.com>
2023-07-07 21:32:29 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/chessgame/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.chessgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.chessgame.data.*;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button start = (Button)findViewById(R.id.startButton);
+        final Button playback = (Button)findViewById(R.id.playbackButton);
+        final Button quit = (Button)findViewById(R.id.exitButton);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        savedGames.context = getApplicationContext();
+        savedGames.readData();
+
+        if(start != null){
+            start.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Bundle bundle = new Bundle();
+                    Intent i = new Intent(MainActivity.this, Game.class);
+                    i.putExtras(bundle);
+                    startActivity(i);
+                }
+            });
+        }
+        if(playback != null){
+            playback.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Bundle bundle = new Bundle();
+                    Intent i = new Intent(MainActivity.this, Games.class);
+                    i.putExtras(bundle);
+                    startActivity(i);
+                }
+            });
+        }
+        if(quit != null){
+            quit.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    finishAffinity();
+                    System.exit(0);
+                }
+            });
+        }
+    }
+}
